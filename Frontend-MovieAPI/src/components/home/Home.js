7d32ab5ef564b543636b161581c1feec2af258d7
André Gonçalves_ <117917238+G-don@users.noreply.github.com>
2023-07-22 20:59:37 +0100

    Delete Frontend-MovieAPI directory

--- Frontend-MovieAPI/src/components/home/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Hero from '../hero/Hero';
-
-const Home = ({ movies, getMovieData }) => {
-  return <Hero movies={movies} getMovieData={getMovieData} />;
-};
-
-export default Home;
